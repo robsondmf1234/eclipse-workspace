03b94dcb1facd534bb725043c7aa21ebcbe832bc
robsondmf1234 <49348939+robsondmf1234@users.noreply.github.com>
2019-04-18 09:10:03 -0300

    Update SaldoInsuficienteException.java
    -Comentários

--- Array/src/SaldoInsuficienteException.java
+++ Array/src/SaldoInsuficienteException.java
@@ -1,4 +1,4 @@
-
+//Classe criada para gerar erro referente a saldo insuficiente
 public class SaldoInsuficienteException extends RuntimeException {
 	
 	public SaldoInsuficienteException(String msg) {
