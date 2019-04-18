7314a33a9d476a183c1ba2df6eed2b79cfbd6c87
robsondmf1234 <49348939+robsondmf1234@users.noreply.github.com>
2019-04-18 08:49:40 -0300

    Atualização
    -Comentários

--- Array/src/ContaCorrente.java
+++ Array/src/ContaCorrente.java
@@ -1,23 +1,27 @@
-
+//Clase Conta Corrente
 public class ContaCorrente extends Conta implements Tributavel {
-
+	
+	//Construtor
 	public ContaCorrente(int agencia, int numero) {
 		// Chamando o construtor especifico
 		super(agencia, numero);
 	}
 	
-		@Override
+	//Metodo sobrescrito, será ascredio 20% ao valor ao saque
+	@Override
 	public void saca(double valor) {
 		double valorASacar = valor + 0.2;
 		// chamado o metodo saca da classe Mae
 		super.saca(valorASacar);
 	}
-
+	
+	//Metodo deposita
 	@Override
 	public void deposita(double valor) {
 		super.saldo += valor;
 	}
-
+	
+	//Metodo que recupera o valor do imposto
 	@Override
 	public double getValorImposto() {
 		return super.saldo * 0.01;
