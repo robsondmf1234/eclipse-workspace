3a256fa94afb0067fd820b973c5b2cbaded84e84
robsondmf1234 <49348939+robsondmf1234@users.noreply.github.com>
2019-04-22 15:58:05 -0300

    Update TesteArrayReferencias.java

--- Array/src/TesteArrayReferencias.java
+++ Array/src/TesteArrayReferencias.java
@@ -3,26 +3,23 @@
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
-
-		ContaCorrente[] contas = new ContaCorrente[5];
-		ContaCorrente cc1 = new ContaCorrente(22, 11);
-
-		contas[0] = cc1;
 		
+		//Array de ContaCorrente
+		ContaCorrente[] contas = new ContaCorrente[5];
+		
+		//Instanciando objetos
+		ContaCorrente cc1 = new ContaCorrente(22, 11);
 		ContaCorrente cc2 = new ContaCorrente(22, 22);
+		
+		//Adicionando os objetos ao Array
+		contas[0] = cc1;
 		contas[1] = cc2;
-
+		
+		//Recuperando o numero do objeto da array posição 1
 		System.out.println(contas[1].getNumero());
 		
+		//Atribuindo endereço da memória do objeto conta posição 1 a variavel a ref
 		ContaCorrente ref = contas[1];
 		System.out.println(ref.getNumero());
-		// for (int i = 0; i < idades.length; i++) {
-		// idades[i] = i * i ;
-		// }
-		//
-		// for (int i = 0; i < idades.length; i++) {
-		// System.out.println("Valor: "+idades[i]);
-		// }
 	}
-
 }
