79f18e15458404eb50a8d0fb181db5d2374a7548
robsondmf1234 <49348939+robsondmf1234@users.noreply.github.com>
2019-05-22 08:07:23 -0300

    Update Tributavel.java

--- Array2/src/Tributavel.java
+++ Array2/src/Tributavel.java
@@ -1,6 +1,7 @@
-
+//Interface Tributavel
 public abstract interface Tributavel {
-
+	
+	//método que será implementado pelas classes que extenderem a interface Tributavel
 	public double getValorImposto();
 
 }
