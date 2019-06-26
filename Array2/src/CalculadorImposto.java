e5bfbf45a29f46cc81a33e510ef3c2c4417adfd0
Robson Ferreira <49348939+robsondmf1234@users.noreply.github.com>
2019-06-26 08:35:12 -0300

    Atualização
    Adicionado comentários ao código

--- Array2/src/CalculadorImposto.java
+++ Array2/src/CalculadorImposto.java
@@ -1,13 +1,15 @@
-
+//Classe utilizada para calcular imposto
 public class CalculadorImposto {
 
 	private double totalImposto;
-
+	
+	//método que recebe um objeto tipo Tributavel e acrescenta o valor do imposto
 	public void registra(Tributavel t) {
 		double valor = t.getValorImposto();
 		this.totalImposto += valor;
 	}
-
+	
+	//método que retorna o valor total do imposto
 	public double getTotalImposto() {
 		return totalImposto;
 	}
